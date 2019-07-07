3af4a342aa3c41b6d99f2ba3366d028b4d18ef07
Fotis Papadopoulos <fpapadopou@gmail.com>
2019-07-07 20:51:43 +0300

    Add Polygon validation

--- kmlutils/blocks/polygon.py
+++ kmlutils/blocks/polygon.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import List
 from .point import Point
+from shapely.geometry import Polygon as ShapelyPolygon
 import json
 
 
@@ -22,6 +23,12 @@ class Polygon:
     def get_coordinates(self) -> List[Point]:
         """Returns the current polygon's coordinates."""
         return self._coordinates
+
+    def validate(self):
+        """Validates the current polygon's coordinate list."""
+        polygon = ShapelyPolygon(self._coordinates)
+        if not polygon.is_valid():
+            raise ValueError('Invalid Polygon coordinates: {}'.format(self.serialize()))
 
     def serialize(self) -> str:
         """Returns a JSON representation of the Polygon object"""
